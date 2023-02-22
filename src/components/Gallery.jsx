14379ea8379733ce8da14f95b58f1203766a3cd6
Daniel Soto Delgado <d.sotodelgado@outlook.com>
2023-02-22 14:54:37 +0100

    replace token with .env

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,14 +1,10 @@
 import React, { useState, useEffect } from 'react';
-import pic1 from '../assets/curtain.jpg';
-
 
 function Gallery() {
   const [data, setData] = useState('');
+  const generatedToken = process.env.REACT_APP_TIMECONSTRAINED_TOKEN;
   
-  const generatedToken = 'IGQVJVNWtWbkZAHVHN1N0RIU2pUYWg5bHVhMmdsRFpJVVhRLUZAyd0c0SUZAJR1AtUllaNkk3Ul9HU0F5V0FSSzM2ZAmNzSlFSaGRoRnlBSHpQNk1IRjVfVEVZAMUJqMmZAoR3BjMHM5eTFhTC1PQUNfNkpXNwZDZD';
- 
   useEffect(() => {
-
     const url = `https://graph.instagram.com/me/media?fields=id,media_url,media_type,permalink&access_token=${generatedToken}`;
     
     fetch(url).then(res => res.json()).then(data => {
@@ -17,17 +13,11 @@ function Gallery() {
   }, []);
 
   const getInstagramImages = (data, maxToShow) => {
-    
     let imageArray = data.data; 
-    
-    
-    /* let{media_url}=imageArray[0]; */
-    
-    
-
+  
     if (data) {
       let onlyPics = imageArray.filter(imageArray=>imageArray.media_type==='IMAGE');
-      console.log('onlyPics', onlyPics)
+
       return onlyPics.map((image, index) => {            
         if (index < maxToShow ) {
           return (
@@ -41,15 +31,14 @@ function Gallery() {
           )
         }
       })
-    }
+    } 
   }
 
   return (
     <div className='scroll-item relative gallery scroll-item  h-fit flex justify-center items-center bg-slate-900'>
-     <div className='max-h-full max-w-full grid grid-cols-3 aspect-square grid-rows-3 gap-3 m-auto bg-slate-400
-    '>
+     <div className='max-h-full max-w-full grid grid-cols-3 aspect-square grid-rows-3 gap-3 m-auto bg-slate-400'>
           {getInstagramImages(data, 9)}
-        </div>
+      </div>
     </div>
 
 
