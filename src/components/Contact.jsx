36c964c5bdc17984d0f91c57aba02e2b4bc79380
Daniel Soto Delgado <d.sotodelgado@outlook.com>
2023-03-23 11:10:03 +0100

    add orchestra to bottom

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,11 +3,12 @@ import React, { startTransition } from 'react'
 import {BsInstagram} from 'react-icons/bs' */
 import {AiOutlineInstagram} from 'react-icons/ai'
 import {AiOutlineMail} from 'react-icons/ai'
+import Staatsoper from "../assets/Staatsoper.jpg";
 
 const Contact = () => {
     return (
-        <section id="contact " className=' h-fit flex justify-center relative bg-gradient-to-b from-slate-50 via-slate-200 to-slate-50'>
-            <div className="p-3 flex flex-col max-w-5xl py-44 lg:px-64  place-content-center mx-5 bg-slate-50">
+        <section id="contact " className=' h-fit flex flex-col justify-center relative bg-gradient-to-b from-slate-50 via-slate-200 to-slate-50'>
+            <div className="p-3 flex flex-col max-w-5xl py-32 lg:px-64  place-content-center mx-5 bg-slate-50">
                  <h1 className='text-center text-slate-700 mx-auto m-8 max-w-md'>
                 If you're interested in collaborating or booking, 
                 feel free to write me an email or follow me on Instagram. <br></br>
@@ -24,8 +25,14 @@ const Contact = () => {
                     </a>
                 </div>   
                 </div>
-               
             </div> 
+            <div className="headerContainer overflow-clip relative h-36 flex">
+				{/* Background image */}
+				<img className='absolute bottom-0 overflow-auto w-screen h-auto self-center ' src={Staatsoper} alt="Background" />
+				<div className="absolute bottom-0 bg-slate-50 opacity-80 h-full w-full z-10"></div>
+			</div>
+            
+        
         </section>
     );
 };
